b950cb424a1f8a4325309fdeb911b43db343682a
Brayden Robbins <brayden.robbins@ruralsource.com>
2019-10-02 11:59:25 -0500

    moving everything into the group project repo

---
+++ RSIVueloAPI/RSIVueloAPI/ClientApp/src/Helicopter/helicopter-card.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Meta } = Card;
+
+const HelicopterCard = (props) => {
+
+  return (
+    <>
+      <Link to={{
+        pathname: `/heliDetailPage/${props.helicopter._id}`,
+        state: {
+          helicopter: props.helicopter
+        }
+      }}
+      >
+        < Card
+          hoverable
+          className='helicopter-card'
+          cover={< img alt="example" src={props.helicopter.src} className='helicopter-Img' />}
+        >
+          <Meta title={props.helicopter.model} description={props.helicopter.date} />
+        </Card >
+      </Link>
+    </>
+  )
+}
+
+export default HelicopterCard;
