3e0ad9f6668710e9e3f5ae9c0b4f9161fde72553
henry.nguyen <henry.nguyen@OKCL002>
2019-10-17 13:47:22 -0500

    removed cors

--- RSIVueloAPI/RSIVueloAPI/ClientApp/src/Helicopter/signUp.js
+++ RSIVueloAPI/RSIVueloAPI/ClientApp/src/Helicopter/signUp.js
@@ -42,7 +42,6 @@ function SignUp() {
       headers: { 'Content-Type': 'application/json;charset=UTF-8' },
       accepts: "application/json",
       body: JSON.stringify(newUser),
-      mode: 'no-cors'
     })
       .then(res => {
         clearFields()
